f1c1ff83181465af97a277133a7612f45e5e7079
Vasiliy Basov <vasiliy.basov.82@gmail.com>
2022-11-11 14:39:35 +0300

    Decription added

--- Scripts/test.py
+++ Scripts/test.py
@@ -4,7 +4,7 @@ import unittest
 class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
-        self.assertEqual(1 + 1, 1)
+        self.assertEqual(1 + 1, 1)  # Тест сравнивает два числа в круглых скобках
 
 
 if __name__ == '__main__':
